bb6c2b57ccff6492ce93d848b569569d0e567c6d
princebhayani1902 <princebhayani192003@gmail.com>
2024-06-25 18:36:08 +0530

    add left component

--- src/components/leftComponent/LeftComponent.jsx
+++ src/components/leftComponent/LeftComponent.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import "./LeftComponent.css"
+
 function LeftComponent() {
     return (
-        <div>LeftComponent</div>
+        <div className="left-component">
+            <div className="container">
+                <img src="/public/logo.ico" alt="logo" />
+                <h1>
+                    <span>Code</span> Log
+                </h1>
+                <div className="min-heading">Code. Compile. Debug.</div>
+                <button>
+                    <span>+</span> Create New CodeSpace
+                </button>
+            </div>
+        </div>
     )
 }
 
