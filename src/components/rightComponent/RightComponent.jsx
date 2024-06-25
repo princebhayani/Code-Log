e1752992a58a955b2bb83ae3994afc7fb58d1322
princebhayani1902 <princebhayani192003@gmail.com>
2024-06-25 18:59:41 +0530

    add right component

--- src/components/rightComponent/RightComponent.jsx
+++ src/components/rightComponent/RightComponent.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from "react";
 import "./RightComponent.css";
 
 function RightComponent() {
     return (
-        <div>RightComponent</div>
-    )
+        <div className="right-component">
+            <div className="header">
+                <h3 className="heading">
+                    My <span>CodeSpace</span>
+                </h3>
+                <button>
+                <span>+</span> New Space
+                </button>
+            </div>
+        </div>
+    );
 }
 
-export default RightComponent
+export default RightComponent;
